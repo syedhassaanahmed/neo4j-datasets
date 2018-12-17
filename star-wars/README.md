36c60ffcf7b3a4e01ee62f62340194e8d13d96dc
Syed Hassaan Ahmed <syah@microsoft.com>
2018-12-17 16:55:37 +0100

    Add star wars dataset

---
+++ star-wars/README.md
@@ -0,0 +1,9 @@
+# star-wars
+[![Docker Build Status](https://img.shields.io/docker/build/syedhassaanahmed/neo4j-star-wars.svg?logo=docker)](https://hub.docker.com/r/syedhassaanahmed/neo4j-star-wars/builds/) [![MicroBadger Size](https://img.shields.io/microbadger/image-size/syedhassaanahmed/neo4j-star-wars.svg?logo=docker)](https://hub.docker.com/r/syedhassaanahmed/neo4j-star-wars/tags/) [![Docker Pulls](https://img.shields.io/docker/pulls/syedhassaanahmed/neo4j-star-wars.svg?logo=docker)](https://hub.docker.com/r/syedhassaanahmed/neo4j-star-wars/)
+
+Docker image hosting Neo4j Database of [Wookieepedia](http://starwars.wikia.com/wiki/Main_Page) - the Star Wars Wiki, using [SWAPI.co](https://swapi.co/). The dataset includes People, Planets, Films, Species, Vehicles and Starships.
+
+## Credits
+- **Wookieepedians** for maintaining the online encyclopedia that contains detailed information about the Star Wars movies.
+- **Paul Hallett** and **SWAPI Contributors** for developing the world's first quantified and programmatically-formatted set of Star Wars data. 
+- **William Lyon** for his `Jupyter` Notebook - [Building The Star Wars Graph](https://github.com/johnymontana/SWAPI-graph/blob/master/SWAPI.ipynb).
---
+++ star-wars/swapi.py
@@ -0,0 +1,205 @@
+from py2neo import Graph
+import requests
+import os
+
+graph = Graph("bolt://localhost:7687", auth=("neo4j", os.environ["TEMP_PASSWORD"]))
+
+graph.run("CREATE CONSTRAINT ON (f:Film) ASSERT f.url IS UNIQUE")
+graph.run("CREATE CONSTRAINT ON (p:Person) ASSERT p.url IS UNIQUE")
+graph.run("CREATE CONSTRAINT ON (v:Vehicle) ASSERT v.url IS UNIQUE")
+graph.run("CREATE CONSTRAINT ON (s:Starship) ASSERT s.url IS UNIQUE")
+graph.run("CREATE CONSTRAINT ON (p:Planet) ASSERT p.url IS UNIQUE")
+
+CREATE_PERSON_QUERY = '''
+MERGE (p:Person {url: {url}})
+SET p.birth_year = {birth_year},
+    p.created = {created},
+    p.edited = {edited},
+    p.eye_color = {eye_color},
+    p.gender = {gender},
+    p.hair_color = {hair_color},
+    p.height = {height},
+    p.mass = {mass},
+    p.name = {name},
+    p.skin_color = {skin_color}
+REMOVE p:Placeholder
+WITH p
+MERGE (home:Planet {url: {homeworld}})
+ON CREATE SET home:Placeholder
+CREATE UNIQUE (home)<-[:IS_FROM]-(p)
+WITH p
+UNWIND {species} AS specie
+MERGE (s:Species {url: specie})
+ON CREATE SET s:Placeholder
+CREATE UNIQUE (p)-[:IS_SPECIES]->(s)
+WITH DISTINCT p
+UNWIND {starships} AS starship
+MERGE (s:Starship {url: starship})
+ON CREATE SET s:Placeholder
+CREATE UNIQUE (p)-[:PILOTS]->(s)
+WITH DISTINCT p
+UNWIND {vehicles} AS vehicle
+MERGE (v:Vehicle {url: vehicle})
+ON CREATE SET v:Placeholder
+CREATE UNIQUE (p)-[:PILOTS]->(v)
+'''
+
+CREATE_MOVIE_QUERY = '''
+MERGE (f:Film {url: {url}})
+SET f.created = {created},
+    f.edited = {edited},
+    f.episode_id = toInt({episode_id}),
+    f.opening_crawl = {opening_crawl},
+    f.release_date = {release_date},
+    f.title = {title}
+WITH f
+UNWIND split({director}, ",") AS director
+MERGE (d:Director {name: director})
+CREATE UNIQUE (f)-[:DIRECTED_BY]->(d)
+WITH DISTINCT f
+UNWIND split({producer}, ",") AS producer
+MERGE (p:Producer {name: producer})
+CREATE UNIQUE (f)-[:PRODUCED_BY]->(p)
+WITH DISTINCT f
+UNWIND {characters} AS character
+MERGE (c:Person {url: character})
+ON CREATE SET c:Placeholder
+CREATE UNIQUE (c)-[:APPEARS_IN]->(f)
+WITH DISTINCT f
+UNWIND {planets} AS planet
+MERGE (p:Planet {url: planet})
+ON CREATE SET p:Placeholder
+CREATE UNIQUE (f)-[:TAKES_PLACE_ON]->(p)
+WITH DISTINCT f
+UNWIND {species} AS specie
+MERGE (s:Species {url: specie})
+ON CREATE SET s:Placeholder
+CREATE UNIQUE (s)-[:APPEARS_IN]->(f)
+WITH DISTINCT f
+UNWIND {starships} AS starship
+MERGE (s:Starship {url: starship})
+ON CREATE SET s:Placeholder
+CREATE UNIQUE (s)-[:APPEARS_IN]->(f)
+WITH DISTINCT f
+UNWIND {vehicles} AS vehicle
+MERGE (v:Vehicle {url: vehicle})
+ON CREATE SET v:Placeholder
+CREATE UNIQUE (v)-[:APPEARS_IN]->(f)
+'''
+
+CREATE_PLANET_QUERY = '''
+MERGE (p:Planet {url: {url}})
+SET p.created = {created},
+    p.diameter = {diameter},
+    p.edited = {edited},
+    p.gravity = {gravity},
+    p.name = {name},
+    p.orbital_period = {orbital_period},
+    p.population = {population},
+    p.rotation_period = {rotation_period},
+    p.surface_water = {surface_water}
+REMOVE p:Placeholder
+WITH p
+UNWIND split({climate}, ",") AS c
+MERGE (cli:Climate {type: c})
+CREATE UNIQUE (p)-[:HAS_CLIMATE]->(cli)
+WITH DISTINCT p
+UNWIND split({terrain}, ",") AS t
+MERGE (ter:Terrain {type: t})
+CREATE UNIQUE (p)-[:HAS_TERRAIN]->(ter)
+'''
+
+CREATE_SPECIES_QUERY = '''
+MERGE (s:Species {url: {url}})
+SET s.name = {name},
+    s.language = {language},
+    s.average_height = {average_height},
+    s.average_lifespan = {average_lifespan},
+    s.classification = {classification},
+    s.created = {created},
+    s.designation = {designation},
+    s.eye_colors = {eye_colors},
+    s.hair_colors = {hair_colors},
+    s.skin_colors = {skin_colors}
+REMOVE s:Placeholder
+'''
+
+CREATE_STARSHIP_QUERY = '''
+MERGE (s:Starship {url: {url}})
+SET s.MGLT = {MGLT},
+    s.consumables = {consumables},
+    s.cost_in_credits = {cost_in_credits},
+    s.created = {created},
+    s.crew = {crew},
+    s.edited = {edited},
+    s.hyperdrive_rating = {hyperdrive_rating},
+    s.length = {length},
+    s.max_atmosphering_speed = {max_atmosphering_speed},
+    s.model = {model},
+    s.name = {name},
+    s.passengers = {passengers}
+REMOVE s:Placeholder
+MERGE (m:Manufacturer {name: {manufacturer}})
+CREATE UNIQUE (s)-[:MANUFACTURED_BY]->(m)
+WITH s
+MERGE (c:StarshipClass {type: {starship_class}})
+CREATE UNIQUE (s)-[:IS_CLASS]->(c)
+'''
+
+CREATE_VEHICLE_QUERY = '''
+MERGE (v:Vehicle {url: {url}})
+SET v.cargo_capacity = {cargo_capacity},
+    v.consumables = {consumables},
+    v.cost_in_credits = {cost_in_credits},
+    v.created = {created},
+    v.crew = {crew},
+    v.edited = {edited},
+    v.length = {length},
+    v.max_atmosphering_speed = {max_atmosphering_speed},
+    v.model = {model},
+    v.name = {name},
+    v.passengers = {passengers}
+REMOVE v:Placeholder
+MERGE (m:Manufacturer {name: {manufacturer}})
+CREATE UNIQUE (v)-[:MANUFACTURED_BY]->(m)
+WITH v
+MERGE (c:VehicleClass {type: {vehicle_class}})
+CREATE UNIQUE (v)-[:IS_CLASS]->(c)
+'''
+
+for i in range(1,8):
+    url = "http://swapi.co/api/films/" + str(i) + "/"
+    r = requests.get(url)
+    params = r.json()
+    graph.run(CREATE_MOVIE_QUERY, params)
+    print("Inserted film: " + str(url))
+
+FIND_NEW_ENTITY_QUERY = '''
+MATCH (p:Placeholder)
+WITH rand() AS r, p ORDER BY r LIMIT 1
+WITH p
+RETURN p.url AS url, CASE WHEN head(labels(p))="Placeholder" THEN labels(p)[1] ELSE head(labels(p)) END AS type
+'''
+
+def getQueryForLabel(label):
+    if (label == 'Vehicle'):
+        return CREATE_VEHICLE_QUERY
+    elif (label == 'Species'):
+        return CREATE_SPECIES_QUERY
+    elif (label == 'Person'):
+        return CREATE_PERSON_QUERY
+    elif (label == 'Starship'):
+        return CREATE_STARSHIP_QUERY
+    elif (label == 'Planet'):
+        return CREATE_PLANET_QUERY
+    else:
+        raise ValueError("Unknown label for entity: " + str(label))
+
+result = graph.run(FIND_NEW_ENTITY_QUERY)
+while result.forward():
+    label = result.current["type"]
+    url = result.current["url"]
+    r = requests.get(url)
+    params = r.json()
+    graph.run(getQueryForLabel(label), params)
+    result = graph.run(FIND_NEW_ENTITY_QUERY)
